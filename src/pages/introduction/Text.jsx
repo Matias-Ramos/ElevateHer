826d12f1867abeded4962ef2b53cf8feabbde918
Matias <matiasramos>
2024-01-05 11:53:56 -0300

    isabel font added and imported in Introduction.jsx to test it with the client

--- src/pages/introduction/Text.jsx
+++ src/pages/introduction/Text.jsx
@@ -1,7 +1,8 @@
+import '../../assets/fonts/isabel-black.otf'
 const Text = () => (
     <>
-        <h1>EmpowerHer</h1>
-        <p>Revoluciona tu <u>cuerpo</u>, fortalece tu <u>mente</u> y sana tu relación con la <u>comida</u> en tan solo 12 semanas de compromiso y dedicación.</p>
+        <h1 style={{fontFamily:"isabel-black"}}>EmpowerHer</h1>
+        <p style={{fontFamily:"isabel-black"}}>Revoluciona tu <u>cuerpo</u>, fortalece tu <u>mente</u> y sana tu relación con la <u>comida</u> en tan solo 12 semanas de compromiso y dedicación.</p>
     </>
 )
 export default Text
