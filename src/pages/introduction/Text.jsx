ae8fa60ecdb21dfecaafdd42700eb7e132bb9370
Matias <matiasramos>
2024-01-05 23:59:04 -0300

    trying to implement Isabel font. Works on dev, not on prod.

--- src/pages/introduction/Text.jsx
+++ src/pages/introduction/Text.jsx
@@ -1,8 +1,7 @@
-import '../../assets/fonts/isabel-black.otf'
 const Text = () => (
     <>
-        <h1 style={{fontFamily:"isabel-black"}}>EmpowerHer</h1>
-        <p style={{fontFamily:"isabel-black"}}>Revoluciona tu <u>cuerpo</u>, fortalece tu <u>mente</u> y sana tu relación con la <u>comida</u> en tan solo 12 semanas de compromiso y dedicación.</p>
+        <h1>EmpowerHer</h1>
+        <p>Revoluciona tu <u>cuerpo</u>, fortalece tu <u>mente</u> y sana tu relación con la <u>comida</u> en tan solo 12 semanas de compromiso y dedicación.</p>
     </>
 )
 export default Text
