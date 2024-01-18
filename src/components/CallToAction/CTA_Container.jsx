4324cf78b73f91fe9fe2d492e47f0a52ba6d0e19
Matias <matiasramos>
2024-01-18 10:16:38 -0300

    btns animate every time they are on viewport

--- src/components/CallToAction/CTA_Container.jsx
+++ src/components/CallToAction/CTA_Container.jsx
@@ -6,6 +6,8 @@ import Row from 'react-bootstrap/Row';
 import FormattedText from './FormattedText';
 import ScheduleBtn from './ScheduleBtn';
 import BuyBtn from './BuyBtn';
+// Animation
+import { motion } from 'framer-motion';
 // Styling
 const rowStyling = {
     display: "flex",
@@ -25,32 +27,41 @@ const btnsStyling = {
     gap: "10px",
 }
 
-const CTA_Container = ({ stripeLink, text, isCTAsection = false }) => (
-    <Container className="callToAction">
-        <Row style={rowStyling}>
+const CTA_Container = ({ stripeLink, text, isCTAsection = false }) => {
 
-            <Col
-                className='ctaCol'
-                style={motivationalTxtStyling}
-                xs={12}
-                md={10}
-                lg={12}
-            >
-                <FormattedText text={text} />
-            </Col>
 
-            <Col
-                className='ctaCol'
-                style={btnsStyling}
-                xs={12}
-                md={10}
-                lg={isCTAsection ? 6 : 10}
-            >
-                <ScheduleBtn />
-                <BuyBtn stripeLink={stripeLink} />
-            </Col>
-        </Row>
-    </Container>
-)
+    return(
+        <Container className="callToAction">
+            <Row style={rowStyling}>
+
+                <Col
+                    className='ctaCol'
+                    style={motivationalTxtStyling}
+                    xs={12}
+                    md={10}
+                    lg={12}
+                >
+                    <FormattedText text={text} />
+                </Col>
+
+                <Col
+                    className='ctaCol'
+                    xs={12}
+                    md={10}
+                    lg={isCTAsection ? 6 : 10}
+                >
+                    <motion.div
+                        whileInView={{ scale: [1, 1.03, 1, 1.03, 1] } }
+                        transition={{ delay: 0.4, duration: 0.75, ease: "easeInOut" }}
+                        style={btnsStyling}
+                    >
+                        <ScheduleBtn />
+                        <BuyBtn stripeLink={stripeLink} />
+                    </motion.div>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
 
 export default CTA_Container
